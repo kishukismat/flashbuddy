99e2802191eb19d30ffc0fb1e810b732d8845b65
john.leidel@gmail.com <john.leidel@gmail.com@3ad34b6f-bb32-8f00-f4d8-06e690a498ec>
2014-03-02 22:43:09 +0000

    adding initial layout for authentication; user+password fields + buttons for authenticating and creating a new user account

--- trunk/FlashBuddy/gen/com/example/flashbuddy/R.java
+++ trunk/FlashBuddy/gen/com/example/flashbuddy/R.java
@@ -24,7 +24,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int LogInBanner=0x7f080002;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080004;
+        public static final int button2=0x7f080003;
+        public static final int password=0x7f080001;
+        public static final int username=0x7f080000;
     }
     public static final class layout {
         public static final int activity_flash_buddy=0x7f030000;
@@ -35,7 +40,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int create_flash_decks=0x7f050008;
+        public static final int create_new_account=0x7f050006;
+        public static final int log_in=0x7f050005;
+        public static final int log_in_banner=0x7f050002;
+        public static final int password=0x7f050004;
+        public static final int share_flash_decks=0x7f050009;
+        public static final int username=0x7f050003;
+        public static final int view_flash_decks=0x7f050007;
     }
     public static final class style {
         /** 
