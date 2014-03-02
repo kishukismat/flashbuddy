213d774a38c0789c8d8853a43ca5e21e12955d53
john.leidel <john.leidel@gmail.com>
2014-03-02 23:35:57 +0000

    ...yet another fix to the bloody layout

--- FlashBuddy/gen/com/example/flashbuddy/R.java
+++ FlashBuddy/gen/com/example/flashbuddy/R.java
@@ -25,10 +25,9 @@ public final class R {
     }
     public static final class id {
         public static final int LogInBanner=0x7f080002;
-        public static final int action_settings=0x7f080006;
-        public static final int button1=0x7f080005;
-        public static final int button2=0x7f080003;
-        public static final int password=0x7f080004;
+        public static final int action_settings=0x7f080005;
+        public static final int button1=0x7f080003;
+        public static final int button2=0x7f080004;
         public static final int password_text=0x7f080001;
         public static final int username_text=0x7f080000;
     }
