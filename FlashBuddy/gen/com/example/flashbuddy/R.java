faa4193af1bb7d1e0271b9c4820ede2ce5a2e331
john.leidel <john.leidel@gmail.com>
2014-03-03 00:41:46 +0000

    updating main user activity to display failed message using the new DisplayedMessageActivity; adding the FlashBuddyDisplayedMessageActivity & the FlashBuddyUserActivity; the latter will serve as the basis for the main user UI.

--- FlashBuddy/gen/com/example/flashbuddy/R.java
+++ FlashBuddy/gen/com/example/flashbuddy/R.java
@@ -33,19 +33,26 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_flash_buddy=0x7f030000;
+        public static final int activity_flash_buddy_displayed_message=0x7f030001;
+        public static final int activity_flash_buddy_user=0x7f030002;
     }
     public static final class menu {
         public static final int flash_buddy=0x7f070000;
+        public static final int flash_buddy_displayed_message=0x7f070001;
+        public static final int flash_buddy_user=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int create_flash_decks=0x7f050008;
         public static final int create_new_account=0x7f050006;
+        public static final int hello_world=0x7f05000b;
         public static final int log_in=0x7f050005;
         public static final int log_in_banner=0x7f050002;
         public static final int password=0x7f050004;
         public static final int share_flash_decks=0x7f050009;
+        public static final int title_activity_flash_buddy_displayed_message=0x7f05000a;
+        public static final int title_activity_flash_buddy_user=0x7f05000c;
         public static final int username=0x7f050003;
         public static final int view_flash_decks=0x7f050007;
     }
