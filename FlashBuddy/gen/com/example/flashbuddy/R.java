3fc7d5a3a3d0dcae9624118e65a90640f5b55c4c
john.leidel <john.leidel@gmail.com>
2014-03-05 03:30:41 +0000

    updating the deck code from zack's commit

--- FlashBuddy/gen/com/example/flashbuddy/R.java
+++ FlashBuddy/gen/com/example/flashbuddy/R.java
@@ -35,24 +35,23 @@ public final class R {
         public static final int CreateUserBanner=0x7f090008;
         public static final int DeleteDeck=0x7f09000b;
         public static final int EditDeck=0x7f09000a;
-        public static final int Help=0x7f090010;
+        public static final int Help=0x7f09000f;
         public static final int LogInBanner=0x7f090002;
-        public static final int Logout=0x7f090011;
-        public static final int ModifyFileList=0x7f09000d;
-        public static final int StartStudying=0x7f09000f;
-        public static final int ViewDecks=0x7f09000e;
-        public static final int action_settings=0x7f090016;
+        public static final int Logout=0x7f090010;
+        public static final int StartStudying=0x7f09000e;
+        public static final int ViewDecks=0x7f09000d;
+        public static final int action_settings=0x7f090015;
         public static final int button1=0x7f090003;
         public static final int button2=0x7f090004;
         public static final int create_password_text_1=0x7f090005;
         public static final int create_password_text_2=0x7f090007;
         public static final int create_user_button=0x7f090009;
         public static final int create_username_text=0x7f090006;
-        public static final int current_user=0x7f090012;
+        public static final int current_user=0x7f090011;
         public static final int password_text=0x7f090001;
-        public static final int tvChild=0x7f090014;
-        public static final int tvGroup=0x7f090015;
-        public static final int usernameTextview=0x7f090013;
+        public static final int tvChild=0x7f090013;
+        public static final int tvGroup=0x7f090014;
+        public static final int usernameTextview=0x7f090012;
         public static final int username_text=0x7f090000;
     }
     public static final class layout {
