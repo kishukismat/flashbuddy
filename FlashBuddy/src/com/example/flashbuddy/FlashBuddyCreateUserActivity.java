8850b11289da35b71e204b0629e10402602d1ba0
ttu.baker <ttu.baker@gmail.com>
2014-03-05 05:11:10 +0000

    Added authentication to the application.  Added Create User functionality to the application.  Added specific failure messages to application during authentication.  NOTE:  Default login information:  username: "admin", password: "password"

--- FlashBuddy/src/com/example/flashbuddy/FlashBuddyCreateUserActivity.java
+++ FlashBuddy/src/com/example/flashbuddy/FlashBuddyCreateUserActivity.java
@@ -16,11 +16,17 @@ import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Build;
 
 public class FlashBuddyCreateUserActivity extends Activity {
+	public final static String AUTH_MESSAGE = "com.example.flashbuddy.AUTHENTICATION";
+	public static String PREF_FILE_NAME = "default";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -52,6 +58,48 @@ public class FlashBuddyCreateUserActivity extends Activity {
 	 * @param view the current view pane
 	 */
 	public void onClickCreateUser(View view){
+		EditText username = (EditText)findViewById(R.id.create_username_text);
+    	EditText password = (EditText)findViewById(R.id.create_password_text_1);
+    	EditText confirmPassword = (EditText)findViewById(R.id.create_password_text_2);
+    	
+    	String enteredUsername = username.getText().toString();
+		String enteredPassword = password.getText().toString();
+		String enteredPassword2 = confirmPassword.getText().toString();
+    	final String Username = "usernameKey"; 
+    	final String Password = "passwordKey"; 
+		
+    	
+    	if(enteredPassword.equals(enteredPassword2))
+    	{
+    		// Sets up the new user's shared preferences
+    		FlashBuddyCreateUserActivity.PREF_FILE_NAME = enteredUsername;
+    		//PREF_FILE_NAME = enteredUsername;
+        	SharedPreferences sharedPrefs = getApplicationContext().getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
+        	SharedPreferences.Editor editor = sharedPrefs.edit();
+        	editor.putString(Username, enteredUsername);
+        	editor.putString(Password, enteredPassword);
+        	editor.commit();
+        	
+        	try
+        	{ 
+        		Intent leave = new Intent(FlashBuddyCreateUserActivity.this, FlashBuddy.class);
+        		startActivity(leave);
+        	}catch(Exception e){
+        		
+        	}
+
+    	}
+    	else
+    	{		
+    		Intent failedIntent = new Intent( this, FlashBuddyDisplayedMessageActivity.class );
+    		SharedPreferences settings = getSharedPreferences("admin", 
+    			    Context.MODE_PRIVATE);
+    			String myString = settings.getString(Password, "");
+        	String failedMessage = myString;
+        	//String failedMessage = "Passwords do not match!  Please hit the back button and try again.";
+        	failedIntent.putExtra( AUTH_MESSAGE, failedMessage);
+        	startActivity(failedIntent);
+    	}
 		
 	}
 	
