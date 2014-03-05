6de89f80b51ffc2ee00fd93a829bde07fea56b29
john.leidel <john.leidel@gmail.com>
2014-03-05 03:03:55 +0000

    updating strings value to include color

--- FlashBuddy/gen/com/example/flashbuddy/R.java
+++ FlashBuddy/gen/com/example/flashbuddy/R.java
@@ -10,6 +10,13 @@ package com.example.flashbuddy;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int Black=0x7f060000;
+        public static final int ExpandChildBackground=0x7f060001;
+        public static final int ExpandGroupBackground=0x7f060002;
+        public static final int highlighted_entry=0x7f060003;
+        public static final int transparent=0x7f060004;
+    }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
 
@@ -24,26 +31,28 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int CreateDeck=0x7f08000c;
-        public static final int CreateUserBanner=0x7f080008;
-        public static final int DeleteDeck=0x7f08000b;
-        public static final int EditDeck=0x7f08000a;
-        public static final int Help=0x7f08000f;
-        public static final int LogInBanner=0x7f080002;
-        public static final int Logout=0x7f080010;
-        public static final int StartStudying=0x7f08000e;
-        public static final int ViewDecks=0x7f08000d;
-        public static final int action_settings=0x7f080013;
-        public static final int button1=0x7f080003;
-        public static final int button2=0x7f080004;
-        public static final int create_password_text_1=0x7f080005;
-        public static final int create_password_text_2=0x7f080007;
-        public static final int create_user_button=0x7f080009;
-        public static final int create_username_text=0x7f080006;
-        public static final int current_user=0x7f080011;
-        public static final int password_text=0x7f080001;
-        public static final int usernameTextview=0x7f080012;
-        public static final int username_text=0x7f080000;
+        public static final int CreateDeck=0x7f09000c;
+        public static final int CreateUserBanner=0x7f090008;
+        public static final int DeleteDeck=0x7f09000b;
+        public static final int EditDeck=0x7f09000a;
+        public static final int Help=0x7f09000f;
+        public static final int LogInBanner=0x7f090002;
+        public static final int Logout=0x7f090010;
+        public static final int StartStudying=0x7f09000e;
+        public static final int ViewDecks=0x7f09000d;
+        public static final int action_settings=0x7f090015;
+        public static final int button1=0x7f090003;
+        public static final int button2=0x7f090004;
+        public static final int create_password_text_1=0x7f090005;
+        public static final int create_password_text_2=0x7f090007;
+        public static final int create_user_button=0x7f090009;
+        public static final int create_username_text=0x7f090006;
+        public static final int current_user=0x7f090011;
+        public static final int password_text=0x7f090001;
+        public static final int tvChild=0x7f090013;
+        public static final int tvGroup=0x7f090014;
+        public static final int usernameTextview=0x7f090012;
+        public static final int username_text=0x7f090000;
     }
     public static final class layout {
         public static final int activity_flash_buddy=0x7f030000;
@@ -51,13 +60,15 @@ public final class R {
         public static final int activity_flash_buddy_displayed_message=0x7f030002;
         public static final int activity_flash_buddy_modify_decks=0x7f030003;
         public static final int activity_flash_buddy_user=0x7f030004;
+        public static final int expandlist_child_item=0x7f030005;
+        public static final int expandlist_group_item=0x7f030006;
     }
     public static final class menu {
-        public static final int flash_buddy=0x7f070000;
-        public static final int flash_buddy_create_user=0x7f070001;
-        public static final int flash_buddy_displayed_message=0x7f070002;
-        public static final int flash_buddy_modify_decks=0x7f070003;
-        public static final int flash_buddy_user=0x7f070004;
+        public static final int flash_buddy=0x7f080000;
+        public static final int flash_buddy_create_user=0x7f080001;
+        public static final int flash_buddy_displayed_message=0x7f080002;
+        public static final int flash_buddy_modify_decks=0x7f080003;
+        public static final int flash_buddy_user=0x7f080004;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
@@ -110,10 +121,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
