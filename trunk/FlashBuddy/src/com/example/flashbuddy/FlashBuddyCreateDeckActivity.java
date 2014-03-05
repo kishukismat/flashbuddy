e9366e97af3a7924c72bb9d443fdb049974ba02b
john.leidel@gmail.com <john.leidel@gmail.com@3ad34b6f-bb32-8f00-f4d8-06e690a498ec>
2014-03-05 04:15:42 +0000

    adding createdecks activity

---
+++ trunk/FlashBuddy/src/com/example/flashbuddy/FlashBuddyCreateDeckActivity.java
@@ -0,0 +1,65 @@
+/**
+ * FlashBuddy Create Deck Activity
+ * 
+ * FlashBuddy Create Deck Activity Class 
+ * 
+ * @author John Leidel
+ * @author Zack Falgout
+ * @author Chase Baker 
+ * @version 1.0
+ * 
+ */
+
+package com.example.flashbuddy;
+
+import com.example.flashbuddy.*;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+
+public class FlashBuddyCreateDeckActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_flash_buddy_create_deck);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.flash_buddy_create_deck, menu);
+		return true;
+	}
+	
+	public void onClickCreateDeck(View view){
+	
+		EditText name = (EditText)findViewById(R.id.create_deck_name);
+		EditText subject = (EditText)findViewById(R.id.create_deck_subject);
+		EditText numcards = (EditText)findViewById(R.id.create_deck_numcards);
+		int numCardsInt = Integer.parseInt(numcards.getText().toString());
+		
+		/* 
+		 * create the new deck
+		 */
+		FlashBuddyDeck deck = new FlashBuddyDeck( name.getText().toString(), 
+										subject.getText().toString() );
+		
+		/* 
+		 * for numcards, create the new cards
+		 */
+		
+		/* 
+		 * write the deck out to a file 
+		 */
+		deck.writeDeck();
+		
+		Intent returnUserIntent = new Intent( this, FlashBuddyUserActivity.class );
+    	startActivity(returnUserIntent);
+	}
+
+}
