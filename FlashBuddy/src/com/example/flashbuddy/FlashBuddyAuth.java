5f4606b386136af111f364263357b274cf5ad580
ttu.baker <ttu.baker@gmail.com>
2014-04-02 01:02:49 +0000

    Gesture Support!  Basic skeletons for gesture support added.  See the Login Screen to test gestures in action!  Swipe up to see help, Swipe down to go back.  Easy as that!  Will be adding more in the future...

--- FlashBuddy/src/com/example/flashbuddy/FlashBuddyAuth.java
+++ FlashBuddy/src/com/example/flashbuddy/FlashBuddyAuth.java
@@ -12,7 +12,12 @@
 
 package com.example.flashbuddy;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.util.Log;
+import android.widget.EditText;
 
 
 public class FlashBuddyAuth {
