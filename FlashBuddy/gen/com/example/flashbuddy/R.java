28b6ea1ec0e269e7a37d79bd6f44d18244706ea2
ZFalgout <zfalgout@gmail.com>
2014-05-09 23:29:51 +0000

    Started to add new timer functionality.  Timer button, deleted the old Sess_Timer class.  As of right now flashbuddy creashes upon timer button press.  I'm still figuring it out as I've never made any buttons on android before.  Revamped the session_timer.xml file so it actually looks good.  Now I just have to figure out how to get the timer button to launch the FlashBuddyTimerActivity.

--- FlashBuddy/gen/com/example/flashbuddy/R.java
+++ FlashBuddy/gen/com/example/flashbuddy/R.java
@@ -25,6 +25,7 @@ public final class R {
         public static final int hint=0x7f060009;
         public static final int lightblue=0x7f060000;
         public static final int red=0x7f060001;
+        public static final int timerColor=0x7f060010;
         public static final int transparent=0x7f06000f;
         public static final int white=0x7f06000b;
     }
@@ -54,18 +55,18 @@ public final class R {
         public static final int DeleteDeck=0x7f090020;
         public static final int EditDeck=0x7f090022;
         public static final int LogInBanner=0x7f090004;
-        public static final int Logout=0x7f090027;
+        public static final int Logout=0x7f090025;
         public static final int ModifyFileList=0x7f09001e;
         public static final int ShareDeck=0x7f09001f;
-        public static final int StartStudying=0x7f090026;
+        public static final int StartStudying=0x7f090028;
         public static final int StudyDeck=0x7f090024;
         public static final int StudyFileList=0x7f090023;
-        public static final int ViewDecks=0x7f090025;
-        public static final int action_settings=0x7f090030;
+        public static final int Timer=0x7f09002a;
+        public static final int ViewDecks=0x7f090029;
+        public static final int action_settings=0x7f09002d;
         public static final int answerEntry=0x7f09000a;
         public static final int answerText=0x7f09001b;
         public static final int answerView=0x7f090009;
-        public static final int button=0x7f09002d;
         public static final int button1=0x7f090001;
         public static final int button2=0x7f090000;
         public static final int cardDesignator=0x7f090006;
@@ -77,24 +78,21 @@ public final class R {
         public static final int create_password_text_2=0x7f090014;
         public static final int create_user_button=0x7f090016;
         public static final int create_username_text=0x7f090018;
-        public static final int current_user=0x7f090028;
+        public static final int current_user=0x7f090026;
         public static final int doneButton=0x7f09000e;
         public static final int imageView1=0x7f090019;
-        public static final int instruction=0x7f09002f;
         public static final int nextButton=0x7f09000d;
         public static final int password_text=0x7f090002;
         public static final int questionEntry=0x7f090008;
         public static final int questionText=0x7f09001a;
         public static final int questionView=0x7f090007;
         public static final int textView1=0x7f090005;
-        public static final int time=0x7f09002e;
-        public static final int timer=0x7f09002c;
         public static final int timerEntry=0x7f09000c;
         public static final int timerText=0x7f09001d;
         public static final int timerView=0x7f09000b;
-        public static final int tvChild=0x7f09002a;
-        public static final int tvGroup=0x7f09002b;
-        public static final int usernameTextview=0x7f090029;
+        public static final int tvChild=0x7f09002b;
+        public static final int tvGroup=0x7f09002c;
+        public static final int usernameTextview=0x7f090027;
         public static final int username_text=0x7f090003;
     }
     public static final class layout {
@@ -125,7 +123,6 @@ public final class R {
         public static final int flash_buddy_modify_decks=0x7f080007;
         public static final int flash_buddy_study_decks=0x7f080008;
         public static final int flash_buddy_user=0x7f080009;
-        public static final int xmlparse=0x7f08000a;
     }
     public static final class string {
         public static final int action_settings=0x7f050006;
@@ -165,6 +162,8 @@ public final class R {
         public static final int share_flash_decks=0x7f050018;
         public static final int show_answer_text=0x7f050005;
         public static final int study_deck_button=0x7f05002e;
+        public static final int timer=0x7f050032;
+        public static final int timerPrompt=0x7f050033;
         public static final int title_activity_flash_buddy_card_input=0x7f050030;
         public static final int title_activity_flash_buddy_create_deck=0x7f05002d;
         public static final int title_activity_flash_buddy_create_user=0x7f05001d;
